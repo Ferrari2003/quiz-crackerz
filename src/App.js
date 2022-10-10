30842756ba8c70dc042d96d015035d74b8932b4b
Saiful Islam <https://github.com/Ferrari2003>
2022-10-11 02:54:49 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Blog from './components/Blog/Blog';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './layout/Main';
 
-function App() {
+
+const App = () => {
+ const router = createBrowserRouter([
+   {
+    path:'/',
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/topics',
+        element: <Topics></Topics>
+      },
+      {
+        path: '/statistics',
+        element:<Statistics></Statistics>
+      },
+      {
+        path:'/blog',
+        element: <Blog></Blog>
+      }
+    ]
+   }
+ ]);
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Topics/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+           <h1>My Quiz</h1>
+           <nav>
+            <NavLink   className={({ isActive }) => isActive ? 'active' : undefined  } to={'/topics'}>Topics</NavLink>
+            <NavLink   className={({ isActive }) => isActive ? 'active' : undefined  } to={'/statistics'}>Statistics</NavLink>
+            <NavLink   className={({ isActive }) => isActive ? 'active' : undefined  } to={'/blog'}>Blog</NavLink>
+           </nav>
+        </div>
+    );
+};
+
+export default Header;
